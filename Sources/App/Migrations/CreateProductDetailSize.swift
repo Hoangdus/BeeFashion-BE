aa797237af68ae1f6a449c36177479fb1f06d93c
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-03-03 16:30:30 +0700

    Update migrations

---
+++ Sources/App/Migrations/CreateProductDetailSize.swift
@@ -0,0 +1,25 @@
+//
+//  CreateProductDetailSize.swift
+//
+//
+//  Created by HoangDus on 03/03/2025.
+//
+
+import Fluent
+
+struct CreateProductDetailSize: AsyncMigration {
+	func prepare(on database: Database) async throws {
+		try await database.schema("product_detail+size")
+			.id()
+			.field("product_detail_id", .uuid, .required, .references("product_details", "id"))
+			.field("size_id", .uuid, .required, .references("sizes", "id"))
+			.unique(on: "product_detail_id", "size_id")
+			.field("create_at", .date)
+			.field("update_at", .date)
+			.create()
+	}
+
+	func revert(on database: Database) async throws {
+		try await database.schema("product_detail+size").delete()
+	}
+}
