6e2e63d95cbf396434e216d0414e98d531bbc7df
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-03-27 17:00:19 +0700

    fix deleting item from cart

--- Sources/App/Controllers/CartController.swift
+++ Sources/App/Controllers/CartController.swift
@@ -12,26 +12,26 @@ struct CartController: RouteCollection {
 	func boot(routes: RoutesBuilder) throws {
 		let carts = routes.grouped("carts")
 		
-		carts.group(":customerId"){
+		carts.group(":customerID"){
 			$0.get(use: self.index)
 		}
 		
-		carts.group(":customerId", ":productId"){
+		carts.group(":customerID", ":productID"){
 			$0.post(use: self.create)
-			$0.delete(use: self.delete)
+			$0.delete(":sizeID", use: self.delete)
 			$0.put(use: self.update)
 		}
 	}
 
 	@Sendable
 	func index(req: Request) async throws -> [CartDTO] {
-		guard let customerId: UUID = req.parameters.get("customerId") else { throw Abort(.badRequest) }
+		guard let customerID: UUID = req.parameters.get("customerID") else { throw Abort(.badRequest) }
 		
-		guard let customer = try await Customer.query(on: req.db).with(\.$cartProducts).filter(\.$id == customerId).first() else {
+		guard let customer = try await Customer.query(on: req.db).with(\.$cartProducts).filter(\.$id == customerID).first() else {
 			throw Abort(.notFound)
 		}
 		
-		let carts = try await Cart.query(on: req.db).filter(\.$customer.$id == customerId).all()
+		let carts = try await Cart.query(on: req.db).filter(\.$customer.$id == customerID).all()
 		var cartDTOs: [CartDTO] = []
 		
 		for cart in carts {
@@ -55,16 +55,16 @@ struct CartController: RouteCollection {
 
 	@Sendable
 	func create(req: Request) async throws -> HTTPStatus {
-		guard let customerId: UUID = req.parameters.get("customerId") else { throw Abort(.badRequest) }
-		guard let productId: UUID = req.parameters.get("productId") else { throw Abort(.badRequest) }
+		guard let customerID: UUID = req.parameters.get("customerID") else { throw Abort(.badRequest) }
+		guard let productID: UUID = req.parameters.get("productID") else { throw Abort(.badRequest) }
 		
 		let cartDTO = try req.content.decode(CartDTO.self)
 		
-		guard let product = try await Product.find(productId, on: req.db) else { throw Abort(.notFound) }
-		guard let customer = try await Customer.find(customerId, on: req.db) else { throw Abort(.notFound) }
+		guard let product = try await Product.find(productID, on: req.db) else { throw Abort(.notFound) }
+		guard let customer = try await Customer.find(customerID, on: req.db) else { throw Abort(.notFound) }
 		
 		if(try await customer.$cartProducts.isAttached(to: product, on: req.db)){
-			let cartItem = try await Cart.query(on: req.db).filter(\.$product.$id == productId).filter(\.$customer.$id == customerId).filter(\.$size.$id == cartDTO.sizeID).first()
+			let cartItem = try await Cart.query(on: req.db).filter(\.$product.$id == productID).filter(\.$customer.$id == customerID).filter(\.$size.$id == cartDTO.sizeID).first()
 			if(cartItem != nil){
 				cartItem!.quantity += cartDTO.quantity
 				try await cartItem?.save(on: req.db)
@@ -81,16 +81,16 @@ struct CartController: RouteCollection {
 	
 	@Sendable 
 	func update(req: Request) async throws -> CartDTO {
-		guard let customerId: UUID = req.parameters.get("customerId") else { throw Abort(.badRequest) }
-		guard let productId: UUID = req.parameters.get("productId") else { throw Abort(.badRequest) }
+		guard let customerID: UUID = req.parameters.get("customerID") else { throw Abort(.badRequest) }
+		guard let productID: UUID = req.parameters.get("productID") else { throw Abort(.badRequest) }
 		
 		let cartDTO = try req.content.decode(CartDTO.self)
 		
-		guard let product = try await Product.find(productId, on: req.db) else { throw Abort(.notFound) }
-		guard let customer = try await Customer.find(customerId, on: req.db) else { throw Abort(.notFound) }
+		guard let product = try await Product.find(productID, on: req.db) else { throw Abort(.notFound) }
+		guard let customer = try await Customer.find(customerID, on: req.db) else { throw Abort(.notFound) }
 		
 		if(try await customer.$cartProducts.isAttached(to: product, on: req.db)){
-			let cartItem = try await Cart.query(on: req.db).filter(\.$product.$id == productId).filter(\.$customer.$id == customerId).filter(\.$size.$id == cartDTO.sizeID).first()
+			let cartItem = try await Cart.query(on: req.db).filter(\.$product.$id == productID).filter(\.$customer.$id == customerID).filter(\.$size.$id == cartDTO.sizeID).first()
 			if(cartItem != nil){
 				cartItem!.quantity += cartDTO.quantity
 				if(cartItem!.quantity <= 0){
@@ -107,13 +107,13 @@ struct CartController: RouteCollection {
 
 	@Sendable
 	func delete(req: Request) async throws -> HTTPStatus {
-		guard let customerId: UUID = req.parameters.get("customerId") else { throw Abort(.badRequest) }
-		guard let productId: UUID = req.parameters.get("productId") else { throw Abort(.badRequest) }
+		guard let customerID: UUID = req.parameters.get("customerID") else { throw Abort(.badRequest) }
+		guard let productID: UUID = req.parameters.get("productID") else { throw Abort(.badRequest) }
+		guard let sizeID: UUID = req.parameters.get("sizeID") else { throw Abort(.badRequest) }
 		
-		guard let product = try await Product.find(productId, on: req.db) else { throw Abort(.notFound) }
-		guard let customer = try await Customer.find(customerId, on: req.db) else { throw Abort(.notFound) }
+		guard let cartItem = try await Cart.query(on: req.db).filter(\.$customer.$id == customerID).filter(\.$product.$id == productID).filter(\.$size.$id == sizeID).first() else { throw Abort(.notFound) }
 		
-		try await customer.$cartProducts.detach(product, on: req.db)
+		try await cartItem.delete(on: req.db)
 		return .ok
 	}
 }
